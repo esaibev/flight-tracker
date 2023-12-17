fca73052e0981bf600f3f72f9077d6975006d248
esaibev <esaias.bevegard@gmail.com>
2023-12-17 20:24:37 +0100

    Initial Commit

---
+++ FlightTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlightTracker
+//
+//  Created by Esaias Bevegård on 2023-12-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlightTracker/FlightTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTrackerApp.swift
+//  FlightTracker
+//
+//  Created by Esaias Bevegård on 2023-12-17.
+//
+
+import SwiftUI
+
+@main
+struct FlightTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
